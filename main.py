18d32504753b0f6fc03233ae0a7def18a7fd5559
Chrisnico123 <hutapeniko@gmail.com>
2022-05-13 02:16:37 +0700

    Add Menu, Add Choice Character, and Update Readme

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from ast import Try
+from secrets import choice
 import pygame
 from sys import exit
 
 #class
 class player(pygame.sprite.Sprite):
     max = 0
+    duck = 0
     def __init__(self,image,image1, scale,scale1,jump,jump_scl,slide,slide_scl):
         super().__init__()  
         self.__image = image
@@ -32,10 +33,11 @@ class player(pygame.sprite.Sprite):
         if keys[pygame.K_UP] and player.max < 2 and self.rect.bottom > 705:
             self.gravity = -20
             player.max += 1
-        if keys[pygame.K_DOWN] and self.rect.bottom == 705:
+        if keys[pygame.K_DOWN] and self.rect.bottom == 705 and player.duck < 3:
             self.down = True
             self.gravity = 10   
             self.rect.y = 600
+            player.duck += 1
     def player_gravity(self):
         if self.down:
             self.gravity -= 0.3
@@ -49,6 +51,7 @@ class player(pygame.sprite.Sprite):
             if self.rect.bottom >= 705:
                 self.rect.bottom = 705
                 player.max = 0
+                player.duck = 0
     def animation(self):
         if self.rect.bottom < 705:
             self.img = self.__jump
@@ -67,6 +70,17 @@ class player(pygame.sprite.Sprite):
         self.player_gravity()
         self.animation()
 class player1(player):
+    def __init__(self,image,image1, scale,scale1,jump,jump_scl,slide,slide_scl):
+        super().__init__(image,image1, scale,scale1,jump,jump_scl,slide,slide_scl)
+        self.__image = image
+        self.__image1 = image1
+        self.__jump= jump
+        self.__jump_scl = jump_scl
+        self.__scale1 = scale1
+        self.__scale = scale
+        self.__slide = slide
+        self.__slide_scl = slide_scl
+class player2(player):
     def __init__(self,image,image1, scale,scale1,jump,jump_scl,slide,slide_scl):
         super().__init__(image,image1, scale,scale1,jump,jump_scl,slide,slide_scl)
         self.__image = image
@@ -137,6 +151,7 @@ def HighestScore():
 
 pygame.init()
 game = True
+char = True
 window = pygame.display.set_mode((1200,800))
 pygame.display.set_caption('Last Ninja')
 clock = pygame.time.Clock() 
@@ -159,11 +174,6 @@ bg3 = pygame.transform.scale(background3,(500,250))
 title = fonttitle.render('Last+ninja',False,"black")
 title_rect = title.get_rect(center = (600,100))
 #title end
-
-#score
-# score_text = font.render('Score : 0',False,"white")
-# score_rect = score_text.get_rect(center = (1100,100))
-#score end
 
 #ground
 ground = pygame.image.load('graphic/ground_2/ground 2/ground_2.png').convert_alpha()
@@ -190,7 +200,6 @@ obs4 = pygame.image.load('graphic/grumpy block tilesets game obstacles/PNG/3.png
 obs44 = pygame.transform.scale(obs4 , (90,90))
 obs4_rect = pygame
 #obstacle
-
 #player1
 player_img = pygame.image.load('graphic/NinjaRun/png/Run__004.png').convert_alpha()
 player_img1 = pygame.image.load('graphic/NinjaRun/png/Run__005.png').convert_alpha()
@@ -202,15 +211,29 @@ player_scl = pygame.transform.scale(player_img,(120,140))
 player_scl1 = pygame.transform.scale(player_img1,(120,140))
 player = player1(player_img,player_img1,player_scl,player_scl1,player_jump,player_jump_scl,player_slide,slide_scl)
 #player1 end
+#player2
+player_img_2 = pygame.image.load('graphic/NinjaGirl/png/Run__004.png').convert_alpha()
+player_img1_2 = pygame.image.load('graphic/NinjaGirl/png/Run__005.png').convert_alpha()
+player_jump_2 = pygame.image.load('graphic/NinjaGirl/png/jump__006.png').convert_alpha()
+player_slide_2 = pygame.image.load('graphic/NinjaGirl/png/Slide__009.png').convert_alpha()
+slide_scl_2 = pygame.transform.scale(player_slide_2,(140,95))
+player_jump_scl_2 = pygame.transform.scale(player_jump_2,(120,170))
+player_scl_2 = pygame.transform.scale(player_img_2,(120,140))
+player_scl1_2 = pygame.transform.scale(player_img1_2,(120,140))
+player1 = player2(player_img_2,player_img1_2,player_scl_2,player_scl1_2,player_jump_2,player_jump_scl_2,player_slide_2,slide_scl_2)
+#player2 end
+
 
 player.max = 0
+player1.max = 0
 i=0
 width = 1200
 obs1_x = 500
 obs2_x = 600
-player_gravity = 0
 score_time = 0
 start = False
+choice = False
+duck_max = 0
 while True:
     if start == False:
         for even in pygame.event.get():
@@ -237,21 +260,58 @@ while True:
         window.blit(ground1,(i,700))
         window.blit(ground1,(width+i,700))
         window.blit(title,title_rect)
-        #gambar
-        start_img = pygame.image.load('graphic/NinjaRun/png/Jump__006.png').convert_alpha()
-        start_scl = pygame.transform.scale(player_img,(190,210))
-        window.blit(start_scl,(500,270))
-        #gambar end
-        #start
-        font_start = font.render('Tekan Enter Untuk Memulai Game !!!',False,"white")
-        start_rect = font_start.get_rect(center = (600,550))
-        window.blit(font_start,start_rect)
-        #start end
+        if choice == True:
+            if char == True:
+                #gambar
+                start_img = pygame.image.load('graphic/NinjaRun/png/Jump__006.png').convert_alpha()
+                start_scl = pygame.transform.scale(player_img,(190,210))
+                window.blit(start_scl,(500,270))
+                #gambar end
+            else:
+                #gambar
+                start_img11 = pygame.image.load('graphic/NinjaRun/png/Jump__006.png').convert_alpha()
+                start_scl11 = pygame.transform.scale(player_img_2,(190,210))
+                window.blit(start_scl11,(500,270))
+                #gambar end
+            #start
+            font_start = font.render('Tekan Space Untuk Memulai Game !!!',False,"white")
+            start_rect = font_start.get_rect(center = (600,550))
+            window.blit(font_start,start_rect)
+            #start end
+            keys1 = pygame.key.get_pressed()
+            if keys1[pygame.K_SPACE]:
+                start = True
+        else:
+            choice_char = font.render('Please Choice your Character !!!',False,"white")
+            choice_rect = choice_char.get_rect(center = (600,240))
+            window.blit(choice_char,choice_rect)
+            #Char 1
+            start_img = pygame.image.load('graphic/NinjaRun/png/Jump__006.png').convert_alpha()
+            start_scl = pygame.transform.scale(start_img,(240,300))
+            window.blit(start_scl,(300,270))
+            font_char1 = font.render('Press 1',False,"white")
+            font_rect1 = font_char1.get_rect(center = (420,580))
+            window.blit(font_char1,font_rect1)
+            #char1 end
+
+            #char2   
+            start_img1 = pygame.image.load('graphic/NinjaGirl/png/Jump__006.png').convert_alpha()
+            start_scl1 = pygame.transform.scale(start_img1,(240,300))
+            window.blit(start_scl1,(700,270))
+            font_char2 = font.render('Press 2',False,"white")
+            font_rect2 = font_char2.get_rect(center = (800,580))
+            window.blit(font_char2,font_rect2)
+            #char2 end
+            choice_num = pygame.key.get_pressed()
+            if choice_num[pygame.K_1]:
+                char = True
+                choice = True
+            elif choice_num[pygame.K_2]:
+                char = False
+                choice = True
+
         pygame.display.update()
         clock.tick(60)
-        keys1 = pygame.key.get_pressed()
-        if keys1[pygame.K_UP]:
-            start = True
     else:
         for even in pygame.event.get():
             if even.type == pygame.QUIT:
@@ -259,18 +319,35 @@ while True:
                 exit()
             if game == True:
                 keys = pygame.key.get_pressed()
-                if keys[pygame.K_UP] and player.max < 2:
-                    player.max += 1
-                    player.gravity = -20
-                if keys[pygame.K_DOWN]:
-                    player.gravity = 5
+                if char == True:
+                    if keys[pygame.K_UP] and player.max < 2:
+                            player.max += 1
+                            player.gravity = -20
+                    if keys[pygame.K_DOWN] and player.duck < 3:
+                        if char == True:
+                            player.gravity = 5
+                            player.duck += 1
+                        else:
+                            player1.gravity = 5
+                            player.duck += 1
+                else:
+                    if keys[pygame.K_UP] and player.max < 2:
+                            player.max += 1
+                            player1.gravity = -20
+                    if keys[pygame.K_DOWN] and player.duck < 3:
+                        if char == True:
+                            player.gravity = 5
+                            player.duck += 1
+                        else:
+                            player1.gravity = 5
+                            player.duck += 1
             else:
                 keys1 = pygame.key.get_pressed()
-                if keys1[pygame.K_UP]:
+                if keys1[pygame.K_SPACE]:
                     game = True
                     obs1.rect.right = 800
                     obs2.rect.right = 1200
-                    obs3.rect.right = 1100
+                    obs3.rect.right = 6200
                     score_time = int(pygame.time.get_ticks()/1000)
 
         #reapeat background
@@ -299,10 +376,10 @@ while True:
             obs3.rect.x -= 7
             if obs1.rect.left < -100 : obs1.rect.right = 1300
             if obs2.rect.left < -100 : obs2.rect.right = 1300
-            # if obs3.rect.left < -100 : obs3.rect.right = 1300
+            if obs3.rect.left < -100 : obs3.rect.right = 1300
             window.blit(obs1.get_scale(),obs1.rect)
             window.blit(obs2.get_scale(),obs2.rect)
-            # window.blit(obs3.get_scale(),obs3.rect)
+            window.blit(obs3.get_scale(),obs3.rect)
         #obstacle end
 
         #score
@@ -310,13 +387,12 @@ while True:
         #score end
 
         #player
-            # player_gravity += 1
-            # player.rect.y += player_gravity
-            # if player.rect.bottom >= 705 : 
-            #     player.rect.bottom = 705
-            #     max_jump = 0
-            player.update()
-            window.blit(player.get_transform(),player.rect)
+            if char == True:
+                player.update()
+                window.blit(player.get_transform(),player.rect)
+            else:
+                player1.update()
+                window.blit(player1.get_transform(),player1.rect)
         #player end
 
         #background move
@@ -329,18 +405,17 @@ while True:
                 i=0
             i-=5
         #background end
-
-        #input
-        #keys = pygame.key.get_pressed()
-        #if keys[pygame.K_UP]:
-            #print("loncat")
-        #input end
         else:
-            ninja = pygame.image.load('graphic/NinjaRun/png/Dead__009.png').convert_alpha()
-            ninja_scl = pygame.transform.scale(ninja,(200,200))
-            window.blit(ninja_scl,(500,270))
+            if char == True:
+                ninja = pygame.image.load('graphic/NinjaRun/png/Dead__009.png').convert_alpha()
+                ninja_scl = pygame.transform.scale(ninja,(200,200))
+                window.blit(ninja_scl,(500,270))
+            else:
+                ninja1 = pygame.image.load('graphic/NinjaGirl/png/Dead__009.png').convert_alpha()
+                ninja_scl1 = pygame.transform.scale(ninja1,(200,200))
+                window.blit(ninja_scl1,(500,270))
             popup = font.render(f'& Selamat anda Terlalu NUB untuk game ini &',False,"white")
-            popup1 = font.render(f'Tekan Tombol Arrow UP untuk memulai kembali',False,"white")
+            popup1 = font.render(f'Tekan Tombol Space untuk memulai kembali',False,"white")
             popup_rect = popup.get_rect(center = (600,500))
             popup_rect1 = popup1.get_rect(center = (600,550))
             window.blit(popup,popup_rect)
@@ -348,8 +423,12 @@ while True:
 
 
         #crash player
-        if player.rect.colliderect(obs1.rect) or player.rect.colliderect(obs2.rect):
-            game = False
+        if char == True:
+            if player.rect.colliderect(obs1.rect) or player.rect.colliderect(obs2.rect) or player.rect.colliderect(obs3.rect):
+                game = False
+        else:
+            if player1.rect.colliderect(obs1.rect) or player1.rect.colliderect(obs2.rect) or player1.rect.colliderect(obs3.rect):
+                game = False
         #crash end
 
 
